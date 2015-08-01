a3fc85deb7f6b6b86d1f754eb9ba3ee8aa4101a9
Ioan Loosley <legit.ioangogo@gmail.com>
2015-08-01 17:37:55 +0100

    adding code

---
+++ popup.js
@@ -0,0 +1,55 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+
+
+    var url = tab.url;
+
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+}
+
+/**
+ * @param {string} searchTerm - Search term for Google Image search.
+ * @param {function(string,number,number)} callback - Called when an image has
+ *   been found. The callback gets the URL, width and height of the image.
+ * @param {function(string)} errorCallback - Called when the image is not found.
+ *   The callback gets a string that describes the failure reason.
+ */
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  renderStatus('Making QR code for ' + url);
+  getCurrentTabUrl(function(url) {
+    renderStatus('QR code for ' + url);
+    var imageResult = document.getElementById('image-result');
+    imageResult.width = 200;
+    imageResult.height = 200;
+    imageResult.src = "https://chart.googleapis.com/chart?chs=200x200&cht=qr&choe=UTF-8&chl=" + url;
+    imageResult.hidden = false;
+
+    }, function(errorMessage) {
+      renderStatus('Cannot display image. ' + errorMessage);
+    });
+  });
